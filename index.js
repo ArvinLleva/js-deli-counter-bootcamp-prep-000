ab7dba040943193c90c087b2a863ad0b2b73788f
ArvinLleva <arvin.lleva@yahoo.com>
2018-06-03 02:52:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine(katzDeliLine){
   }
   else{
     for(var i = 0; i < katzDeliLine.length-1; i++){
-    array.push(`${i+1}. ${katzDeliLine[i]}, `)
+    array.push(`${i+1}. ${katzDeliLine[i]} `)
     }
     array.push(` ${katzDeliLine.length}. ${katzDeliLine[katzDeliLine.length-1]}`)
     return "The line is currently: " + array
