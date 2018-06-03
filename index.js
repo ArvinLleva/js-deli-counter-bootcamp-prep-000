ad013964c8b90ebd2f9a369a5d9f045c94196ae8
ArvinLleva <arvin.lleva@yahoo.com>
2018-06-03 02:49:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function currentLine(katzDeliLine){
     return "The line is currently empty."
   }
   else{
-    for(var i = 0; i < katzDeliLine.length; i++){
+    for(var i = 0; i < katzDeliLine.length-1; i++){
     array.push(`${i+1}. ${katzDeliLine[i]}, `)
     }
     return "The line is currently: " + array
