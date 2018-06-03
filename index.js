7fc982b944f3893a4aa08e32c0f8ce131ec73b8e
ArvinLleva <arvin.lleva@yahoo.com>
2018-06-03 00:22:00 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+var katzDeli   = [];
+
+function takeANumber(katzDeli , person){
+  katzDeli.push(person)
+  return `Welcome, ${person}. You are number ${katzDeli.length} in line.`
+}
+
+function nowServing(katzDeli){
+  if(katzDeli.length === 0){
+   return "There is nobody waiting to be served!" 
+  }else 
+  return `Currently serving ${katzDeli[0]}.`
+  katzDeli.shift()
+}
