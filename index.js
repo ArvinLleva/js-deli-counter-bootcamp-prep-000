3f834f82a8662a408438648a316059b01a3f3ffc
ArvinLleva <arvin.lleva@yahoo.com>
2018-06-03 02:13:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,12 +13,13 @@ function nowServing(katzDeliLine){
 }
 
 function currentLine(katzDeliLine){
+  var array = []
   if(katzDeliLine.length === 0){
     return "The line is currently empty."
   }
   else{
     for(var i = 0; i < katzDeliLine.length; i++){
-      
+    
     }
   }
 }
